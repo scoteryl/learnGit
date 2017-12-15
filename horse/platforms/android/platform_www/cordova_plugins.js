df2efc184842c9de31a5e92df00249c405477597
scoteryl <scoteryl@163.com>
2017-12-15 10:12:51 +0800

    2017-12-15 backup app second

---
+++ horse/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,61 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-device.device",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "id": "cordova-plugin-geolocation.geolocation",
+        "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "id": "cordova-plugin-geolocation.PositionError",
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "pluginId": "cordova-plugin-geolocation",
+        "runs": true
+    },
+    {
+        "id": "cordova.custom.plugins.exitapp.exitApp",
+        "file": "plugins/cordova.custom.plugins.exitapp/www/ExitApp.js",
+        "pluginId": "cordova.custom.plugins.exitapp",
+        "merges": [
+            "navigator.app"
+        ]
+    },
+    {
+        "id": "cordova-plugin-dialogs.notification",
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "id": "cordova-plugin-dialogs.notification_android",
+        "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.2",
+    "cordova-plugin-device": "1.1.6",
+    "cordova-plugin-compat": "1.0.0",
+    "cordova-plugin-geolocation": "2.4.3",
+    "cordova.custom.plugins.exitapp": "1.0.0",
+    "cordova-plugin-dialogs": "1.3.3"
+};
+// BOTTOM OF METADATA
+});
